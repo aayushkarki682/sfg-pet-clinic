91c0234f0313bc794d008aa396373903c1fbacd3
aayushkarki682 <ayush.karki12345@gmail.com>
2020-12-31 00:38:23 -0600

    Implement interfaces for service. Closes #5

---
+++ pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package ayush.springframework.sfgpetclinic.services;
+
+import ayush.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package ayush.springframework.sfgpetclinic.services;
+
+import ayush.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
