10f187f9d2b701e040df7a384b5a5a189c9c2998
aayushkarki682 <ayush.karki12345@gmail.com>
2021-01-19 11:10:52 -0600

    Added pettype map service. closes #30

---
+++ pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package ayush.springframework.sfgpetclinic.services;
+
+import ayush.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
