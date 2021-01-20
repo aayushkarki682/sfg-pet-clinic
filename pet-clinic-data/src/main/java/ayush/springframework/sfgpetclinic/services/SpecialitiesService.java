cd96884f8615312e32b3e5c9dd24435e6bf1fbda
aayushkarki682 <ayush.karki12345@gmail.com>
2021-01-20 01:30:26 -0600

    add specialty map service. closes #32

---
+++ pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package ayush.springframework.sfgpetclinic.services;
+
+import ayush.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
