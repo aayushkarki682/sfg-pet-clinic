3aec466f3086acab1aa4f7b288c67f9abd6eb853
aayushkarki682 <ayush.karki12345@gmail.com>
2021-02-05 15:27:01 -0600

    Creating visit entity. closes #39

--- pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package ayush.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "vets")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
