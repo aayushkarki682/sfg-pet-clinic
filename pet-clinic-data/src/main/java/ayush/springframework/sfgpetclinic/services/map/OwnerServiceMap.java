cf84d39c8cfecf87b7015a936f5145fc5ad24157
aayushkarki682 <ayush.karki12345@gmail.com>
2021-02-08 13:50:24 -0600

    profiles updated to use Spring Data JPA service. closes #49

--- pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/ayush/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -5,11 +5,13 @@ import ayush.springframework.sfgpetclinic.model.Pet;
 import ayush.springframework.sfgpetclinic.services.OwnerService;
 import ayush.springframework.sfgpetclinic.services.PetService;
 import ayush.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
